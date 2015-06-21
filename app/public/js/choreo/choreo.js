c505e0b2392a41b770efec49afdabba64b0344ed
alloplastic <sheldon@newlifeinteractive.com>
2015-06-21 11:38:09 -0500

    choreo web framework: array observers added, marshalling of observer events, global var

--- app/public/js/choreo/choreo.js
+++ app/public/js/choreo/choreo.js
@@ -36,23 +36,27 @@ if (!Choreo) {
 
 		var prop, props = propPath.split('.');
 
-		for (var i=0, len=props.length-1; i<len; i+=1) {
-			
+		for (var i=0, len=props.length; i<len; i+=1) {
+
 			prop = props[i];
-			var candidate = obj[prop];
-			if (candidate !== undefined) {
-				obj = candidate;
-			} else {
+
+			// allow array indices as part of the path
+			var index = parseInt(prop);
+			if (index != NaN) prop = index;
+
+			obj= obj[prop];  // TBD: should probably strictly require obj to be an array here
+
+			if (obj == undefined)
 				break;
-			}
 		}
 
-		// TBD: if candidate is an array and the final prop is an int, get by index
-		return obj[props[i]];
+		return obj;
 	};
 
 	// safe, nested access to data, in lieu of automatic data binding
 	Choreo.prototype.set = function(obj, propPath, value) {
+
+		var index;
 
 		if (!propPath)
 			return false;
@@ -62,36 +66,65 @@ if (!Choreo) {
 		for (var i=0, len=props.length-1; i<len; i+=1) {
 			
 			prop = props[i];
-			var candidate = obj[prop];
-			if (candidate !== undefined) {
-				obj = candidate;
-			} else {
+
+			// allow array indices as part of the path
+			index = parseInt(prop);
+			if (index != NaN) prop = index;
+
+			obj = obj[prop];
+
+			if (obj == undefined)
 				return false;
-			}
 		}
 
-		obj[props[i]] = value;
+		var lastProp = props[i];
+		index = parseInt(lastProp);
+		if (index != NaN) lastProp = index;
+
+		obj[lastProp] = value;   // TBD: should probably strictly require obj to be an array here
 
 		this.queueObserverEvent(obj, propPath, value, "set");
-
-		// var self = this;
-		// setTimeout(function () {
-		// 	// Behold, virtual functions in Javascript
-		// 	Choreo.prototype.__proto__.fireObservers(self, obj, propPath, value);
-		// }, 1);   // poor man's event queue
-
-		// TBD: if candidate is an array and the final prop is an int, set by index
 
 		return(true);
 	};
 
 	// remove a property or array item
-	Choreo.prototype.remove = function(obj, propPath, value) {
+	Choreo.prototype.remove = function(obj, propPath) {
 
 		// TBD: same logic as set, supporting arrays, except delete
 
-		this.queueObserverEvent(obj, propPath, value, "remove");
+		if (!propPath)
+			return false;
 
+		var prop = propPath;
+		var lastDelim = prop.lastIndexOf(".");
+
+		// find the parent object of the leaf to be deleted 
+		if (lastDelim >= 0) {
+			prop = propPath.substring(lastDelim+1);
+			var prefixPath = propPath.substring(0, lastDelim);
+			obj = this.get(obj, prefixPath);
+		}
+
+		if (obj && prop && prop.length>0) {
+
+			var index = parseInt(prop);
+			if (index != NaN) {   // integer properties are expected to be array indices
+				if (Array.isArray(obj)) {
+					obj.splice(index, 1);
+				} else {
+					return false;
+				}
+			} else {
+				delete obj[prop];
+			}
+
+		} else {
+			return false;
+		}
+
+		this.queueObserverEvent(obj, propPath, null, "remove");
+		return true;
 	}
 
 	Choreo.prototype.addObserver = function(obj, propPath, listeningObj) {
@@ -137,6 +170,10 @@ if (!Choreo) {
 
 	Choreo.prototype.fireObserverEvents = function() {
 
+		// marshall all events for a particular listening object into a single notification
+		var eventsForEachListener = {};
+		var listener;
+
 		// TBD: possibly optimize by sorting the property lists and looping through once instead of looking up props
 		for (var e=0, numEvents=this.observerEventQueue.length; e<numEvents; e+=1) {
 
@@ -153,11 +190,18 @@ if (!Choreo) {
 						if (listeners == null || listeners.length <= 0) continue;
 
 						for (var i=0, len=listeners.length; i<len; i+=1) {
-							var listener = listeners[i];
-							this.invoke(listener, "onDataChanged", [observerEvent]);
+							listener = listeners[i];
+							if (eventsForEachListener[listener] == null) eventsForEachListener[listener] = [];
+							eventsForEachListener[listener].push(observerEvent);
 						}
 					}
 				}
+			}
+		}
+
+		for (listener in eventsForEachListener) {
+			if (eventsForEachListener.hasOwnProperty(listener)) {
+				this.invoke(listener, "onDataChanged", [eventsForEachListener[listener]]);
 			}
 		}
 
@@ -213,12 +257,14 @@ if (!Choreo) {
 }
 
 if (!window.choreo) window.choreo = new Choreo();
+if (!_c) var _c = window.choreo;  // TBD: is this a good idea?
 
 // TBD: a lot of options here; could at least let calling code set the speed or drive the
 // events synchronously.
 //	var self = this;
 if (!window.choreo.clock) {
 	this.clock = setInterval(function() {
-		window.choreo.__proto__.tick.call(window.choreo);
+		_c.__proto__.tick.call(_c);
+//		window.choreo.__proto__.tick.call(window.choreo);
 	}, 100);
 }
